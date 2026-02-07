4fa0002bec3a3ec3ab8973c899d4be3da91d6d8a
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2026-02-07 16:57:07 -0500

    working on random distribution of ingredients

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pygame 
 from vector import vec, pyVec
+from ingreDistr import IngredientDistribution
 RESOLUTION = vec(500,350)
 SCALE =2
 UPSCALED = RESOLUTION * SCALE
@@ -19,11 +20,13 @@ class Game(object):
             screen.fill((255,255,255))
             pygame.display.flip()
 
+            IngredientDistribution()
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
 
             elapsed_time = (pygame.time.get_ticks() - start_time) / 1000 #in seconds
 
-            if elapsed_time > 5:
+            if elapsed_time > 120:
                 running = False
---
+++ ingreDistr.py
@@ -0,0 +1,6 @@
+import pygame 
+
+class IngredientDistribution(object):
+
+    def __init__(self):
+        pass 
