33c5c065ebf3c7a1dd1d3a038d57ffc25a98803c
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2026-02-25 14:18:50 -0500

    refactored game

--- game.py
+++ game.py
@@ -9,8 +9,13 @@ class Game(object):
     def __init__(self, image_path, points):
         self.image_path = image_path
         self.points = points
+        self.state = IngredientDistribution(image_path)
 
-    def run(self):
-        
-        IngredientDistribution(self.image_path).run()
-        
+    def handleEvent(self, event):
+        self.state.handleEvent(event)
+
+    def update(self, seconds):
+        self.state.update(seconds)
+
+    def draw(self, surface):
+        self.state.draw(surface)
