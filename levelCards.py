b1d86711a916e34963c2113bf9965974e63acd35
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2026-03-24 00:42:35 -0400

    added nori sprite

--- levelCards.py
+++ levelCards.py
@@ -5,7 +5,7 @@ class LevelCards(object):
     def __init__(self):
         self.levelCards = {
             1: [Card("burgerCard.png", 2100), Card("hotdogCard.png", 2000), Card("ramenCard.png", 2300)],
-            2: [Card("pizzaCard.png", 2700), Card("tacoCard.png", 3000)]
+            2: [Card("pizzaCard.png", 2700), Card("tacoCard.png", 3000), Card("sushiCard.png", 2900)]
         }
     
     def get_level_cards(self, level):
