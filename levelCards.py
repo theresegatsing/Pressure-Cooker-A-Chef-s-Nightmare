c167fb3e3db46852516a8ff396713f28b5eb54b2
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2026-04-07 15:52:50 -0400

    level 3 cards

--- levelCards.py
+++ levelCards.py
@@ -5,7 +5,8 @@ class LevelCards(object):
     def __init__(self):
         self.levelCards = {
             1: [Card("burgerCard.png", 2100), Card("hotdogCard.png", 2000), Card("ramenCard.png", 2300)],
-            2: [Card("pizzaCard.png", 2700), Card("tacoCard.png", 3000), Card("sushiCard.png", 2900)]
+            2: [Card("pizzaCard.png", 2700), Card("tacoCard.png", 3000), Card("sushiCard.png", 2900)],
+            3: [Card("croissantCard.png", 3500), Card("friedRiceCard.png", 4000)]
         }
     
     def get_level_cards(self, level):
